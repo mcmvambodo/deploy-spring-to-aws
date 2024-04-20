305b580fe287e539ea19eb8a0b33cc64a23fbe45
michelchateau <135348222+michelchateau@users.noreply.github.com>
2024-04-21 01:45:12 +0200

    New test added

--- src/main/java/doitwell/group/springaws/controllers/AwsController.java
+++ src/main/java/doitwell/group/springaws/controllers/AwsController.java
@@ -1,5 +1,8 @@
 package doitwell.group.springaws.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("aws")
 public class AwsController {
+
+    ObjectMapper mapper = new ObjectMapper();
     @GetMapping
-    public String mbolo(){
-        return "Mbolo oh";
+    public ResponseEntity<String> mbolo() throws JsonProcessingException {
+        return ResponseEntity.ok(mapper.writeValueAsString(new User("mc","mvambodo"))) ;
     }
 }
+
+record User(String username, String password){}
--- src/test/java/doitwell/group/springaws/SpringAwsApplicationTests.java
+++ src/test/java/doitwell/group/springaws/SpringAwsApplicationTests.java
@@ -1,13 +1,40 @@
 package doitwell.group.springaws;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class SpringAwsApplicationTests {
 
-    @Test
+    private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext context;
+    @BeforeEach
     void contextLoads() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
     }
+
+    @Test
+    public void givenString_whenHttpGetAws_thenReturnString() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/aws"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username",is("mc")))
+                .andExpect(jsonPath("$.password",is("mvambodo")));
+    }
+
+
 
 }
